CaTeIM/Jade

multisig: refactor threshold to uint32_t

main/multisig.c
@@ -18,7 +18,7 @@ static const uint8_t CURRENT_MULTISIG_RECORD_VERSION = 3;
 // version 1, 1of1  (moving to v1 predated allowing just 1 signer)
 #define MIN_MULTISIG_BYTES_LEN (4 + 78 + 32)
 
-bool multisig_data_to_bytes(const script_variant_t variant, const bool sorted, const uint8_t threshold,
+bool multisig_data_to_bytes(const script_variant_t variant, const bool sorted, const uint32_t threshold,
     const uint8_t* master_blinding_key, const size_t master_blinding_key_len, const signer_t* signers,
     const size_t num_signers, const size_t total_num_path_elements, uint8_t* output_bytes, const size_t output_len)
 {
@@ -27,6 +27,7 @@ bool multisig_data_to_bytes(const script_variant_t variant, const bool sorted, c
     JADE_ASSERT(signers);
     JADE_ASSERT(num_signers >= threshold);
     JADE_ASSERT(num_signers <= MAX_ALLOWED_SIGNERS);
+    JADE_STATIC_ASSERT(MAX_ALLOWED_SIGNERS <= 0xff); // Must fit in uint8_t
     JADE_ASSERT(total_num_path_elements <= num_signers * 2 * MAX_PATH_LEN);
     JADE_ASSERT(output_bytes);
     JADE_ASSERT(output_len == MULTISIG_BYTES_LEN(master_blinding_key_len, num_signers, total_num_path_elements));
@@ -42,18 +43,13 @@ bool multisig_data_to_bytes(const script_variant_t variant, const bool sorted, c
     write_ptr += sizeof(variant_byte);
 
     // 'sorted' flag (new to version 1)
-    const uint8_t sorted_byte = (uint8_t)sorted;
-    memcpy(write_ptr, &sorted_byte, sizeof(sorted_byte));
-    write_ptr += sizeof(sorted_byte);
+    *write_ptr++ = (uint8_t)sorted;
 
     // Threshold
-    memcpy(write_ptr, &threshold, sizeof(threshold));
-    write_ptr += sizeof(threshold);
+    *write_ptr++ = (uint8_t)threshold;
 
     // Blinding key len, and data (new to version 2)
-    const uint8_t keylen = (uint8_t)master_blinding_key_len;
-    memcpy(write_ptr, &keylen, sizeof(keylen));
-    write_ptr += sizeof(keylen);
+    *write_ptr++ = (uint8_t)master_blinding_key_len;
 
     if (master_blinding_key_len) {
         memcpy(write_ptr, master_blinding_key, master_blinding_key_len);

main/multisig.h
@@ -52,7 +52,7 @@ typedef struct _multisig_data {
     uint8_t xpubs[MAX_ALLOWED_SIGNERS * BIP32_SERIALIZED_LEN];
 } multisig_data_t;
 
-WARN_UNUSED_RESULT bool multisig_data_to_bytes(script_variant_t variant, bool sorted, uint8_t threshold,
+WARN_UNUSED_RESULT bool multisig_data_to_bytes(script_variant_t variant, bool sorted, uint32_t threshold,
     const uint8_t* master_blinding_key, size_t master_blinding_key_len, const signer_t* signers, size_t num_signers,
     size_t total_num_path_elements, uint8_t* output_bytes, size_t output_len);
 

ilkerbbb/piJade

(cherry picked from commit 5aec9db0eda9f7f7a940d6d3ab8191bc059e28b5)
Conflicts: main/multisig.c (three blocks) and main/multisig.h (one).  This fork
had already rewritten the record writer: the function is multisig_body_to_bytes()
rather than upstream's multisig_data_to_bytes(), and it is reached through
multisig_open_registration() and multisig_seal_body(), which upstream does not
have.  Those names and that call chain were kept.
Upstream's side was taken where the record depends on it: the parameter type
(uint32_t), the JADE_STATIC_ASSERT that MAX_ALLOWED_SIGNERS fits in a uint8_t,
and the single-byte writes.  Taking the type on its own would have been silently
wrong.  The fork's body wrote the field with memcpy(write_ptr, &threshold,
sizeof(threshold)), so a wider parameter would have written four bytes where
MULTISIG_BODY_LEN() counts one (its header is 5 * sizeof(uint8_t): variant,
sorted, threshold, keylen, num_signers), pushing every later field along and
breaking a record this fork can still read.  The reader takes a single byte
(multisig.c `output->threshold = *read_ptr`) and was not touched; register_multisig_process()
already refuses a threshold above MAX_ALLOWED_SIGNERS before the writer sees it.
Upstream's "(new to version 1)" and "(new to version 2)" comments were not taken;
this fork carries its own record version.
Measured in the emulator: a registration written by the binary from the commit
before this series is read back by the binary from the end of it, and the
reverse, with threshold 2 and two signers intact in both directions.

main/multisig.c
@@ -34,7 +34,7 @@ bool multisig_open_registration(
     return registration_open(CURRENT_MULTISIG_RECORD_VERSION, bytes, bytes_len, body, body_len, written);
 }
 
-bool multisig_body_to_bytes(const script_variant_t variant, const bool sorted, const uint8_t threshold,
+bool multisig_body_to_bytes(const script_variant_t variant, const bool sorted, const uint32_t threshold,
     const uint8_t* master_blinding_key, const size_t master_blinding_key_len, const signer_t* signers,
     const size_t num_signers, const size_t total_num_path_elements, uint8_t* body, const size_t body_len)
 {
@@ -43,6 +43,7 @@ bool multisig_body_to_bytes(const script_variant_t variant, const bool sorted, c
     JADE_ASSERT(signers);
     JADE_ASSERT(num_signers >= threshold);
     JADE_ASSERT(num_signers <= MAX_ALLOWED_SIGNERS);
+    JADE_STATIC_ASSERT(MAX_ALLOWED_SIGNERS <= 0xff); // Must fit in uint8_t
     JADE_ASSERT(total_num_path_elements <= num_signers * 2 * MAX_PATH_LEN);
     JADE_ASSERT(body);
     JADE_ASSERT(body_len == MULTISIG_BODY_LEN(master_blinding_key_len, num_signers, total_num_path_elements));
@@ -54,18 +55,13 @@ bool multisig_body_to_bytes(const script_variant_t variant, const bool sorted, c
     write_ptr += sizeof(variant_byte);
 
     // 'sorted' flag
-    const uint8_t sorted_byte = (uint8_t)sorted;
-    memcpy(write_ptr, &sorted_byte, sizeof(sorted_byte));
-    write_ptr += sizeof(sorted_byte);
+    *write_ptr++ = (uint8_t)sorted;
 
     // Threshold
-    memcpy(write_ptr, &threshold, sizeof(threshold));
-    write_ptr += sizeof(threshold);
+    *write_ptr++ = (uint8_t)threshold;
 
     // Blinding key len, and data
-    const uint8_t keylen = (uint8_t)master_blinding_key_len;
-    memcpy(write_ptr, &keylen, sizeof(keylen));
-    write_ptr += sizeof(keylen);
+    *write_ptr++ = (uint8_t)master_blinding_key_len;
 
     if (master_blinding_key_len) {
         memcpy(write_ptr, master_blinding_key, master_blinding_key_len);

main/multisig.h
@@ -60,7 +60,7 @@ typedef struct _multisig_data {
     uint8_t xpubs[MAX_ALLOWED_SIGNERS * BIP32_SERIALIZED_LEN];
 } multisig_data_t;
 
-WARN_UNUSED_RESULT bool multisig_body_to_bytes(script_variant_t variant, bool sorted, uint8_t threshold,
+WARN_UNUSED_RESULT bool multisig_body_to_bytes(script_variant_t variant, bool sorted, uint32_t threshold,
     const uint8_t* master_blinding_key, size_t master_blinding_key_len, const signer_t* signers, size_t num_signers,
     size_t total_num_path_elements, uint8_t* body, size_t body_len);
 